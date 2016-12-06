c4d201eab63fc048a2b0c2eac1e2eb33151af077
Taylor Shuff <tkylesh@gmail.com>
2016-12-06 14:02:09 -0600

    diary page now console.logs out user diary.

--- javascripts/controllers/DiaryCtrl.js
+++ javascripts/controllers/DiaryCtrl.js
@@ -1,5 +1,10 @@
 "use strict";
 
-app.controller("DiaryCtrl", function($scope, $rootScope, $location){
+app.controller("DiaryCtrl", function($scope, $rootScope, $location, DiaryFactory){
+	$scope.Diary = {};
 
+	DiaryFactory.getDiary($rootScope.user.uid).then(function(diary) {
+		$scope.Diary = diary;
+		console.log(diary);
+	});
 });
