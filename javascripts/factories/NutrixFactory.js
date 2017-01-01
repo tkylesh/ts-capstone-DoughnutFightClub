c0b66ca381cc8c348a676b98985629f4bd8c7d51
Taylor Shuff <tkylesh@gmail.com>
2017-01-01 17:16:26 -0600

    removed public folder and deploy stuff.  trying to get images working.  will need to temporarily forget about images and move on the getting nutrient info for each search result.

--- javascripts/factories/NutrixFactory.js
+++ javascripts/factories/NutrixFactory.js
@@ -24,6 +24,27 @@ app.factory("NutrixFactory",function($q, $http, FIREBASE_CONFIG, NUTRITIONIXAPIK
 		});
 	};
 
+	let getImages = (searchObject)=> {
+		return $q((resolve,reject) =>{
+			$http({
+				method: 'Post',
+				url:`https://trackapi.nutritionix.com/v2/natural/nutrients?query=${searchObject}`,
+				headers:{
+					"x-app-Id":"e9bfed54",
+					"x-app-key":"8f291f15c2b4327bb1b83d321d95d4da",
+					"x-remote-user-id":"0",
+					"Content-Type":"application/json"
+				}
+			}).then((response) => {
+				console.log('get Image response: ', response);
+				resolve(response);
+			}, (error)=>{
+				console.log('get Image error: ', error);
+				reject(error);
+			});
+		});
+	};
+
 	let getNutrients = (foodId) => {
 		let authPart =`appId=${NUTRITIONIXAPIKEY.appId}&appKey=${NUTRITIONIXAPIKEY.appKey}`;
 		let filter = 'fields=item_name,item_id,brand_name,brand_id,item_type,nf_calories,nf_total_carbohydrate,nf_dietary_fiber,nf_cholesterol,nf_total_fat,nf_sugars,nf_sodium,nf_protein,images_front_full_url,nf_serving_size_qty,nf_serving_size_unit,nf_servings_per_container';
