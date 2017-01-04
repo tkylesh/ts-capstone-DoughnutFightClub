7aff7f9423e7efc8be0798e889ae0b340d95ddf3
Taylor Shuff <tkylesh@gmail.com>
2017-01-04 11:40:03 -0600

    styling the app. also need to add recalculations of nutrients when user deletes foods from diary.

--- javascripts/factories/NutrixFactory.js
+++ javascripts/factories/NutrixFactory.js
@@ -11,7 +11,7 @@ app.factory("NutrixFactory",function($q, $http, FIREBASE_CONFIG, NUTRITIONIXAPIK
 				let authPart =`appId=${NUTRITIONIXAPIKEY.appId}&appKey=${NUTRITIONIXAPIKEY.appKey}`;
 				let filter = 'fields=item_name,item_id,brand_name,brand_id,item_type,nf_calories,nf_total_carbohydrate,nf_dietary_fiber,nf_cholesterol,nf_total_fat,nf_sugars,nf_sodium,nf_protein,images_front_full_url,nf_serving_size_qty,nf_serving_size_unit,nf_servings_per_container';
 
-					$http.get(`https://api.nutritionix.com/v1_1/search/${searchText}?${filter}&${authPart}`)
+					$http.get(`https://api.nutritionix.com/v1_1/search/${searchText}?${filter}&sort={"field": "_score","order": "desc"}&${authPart}`)
 						.success( (response) => {
 						console.log('nutrix response', response);
 						resolve(response.hits);
